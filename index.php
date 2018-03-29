558ab15c7a8dd77628ba955278ec09581c2e2d78
Marcelo Fernandes <marcelo.fernandes@inffel.com>
2018-03-29 18:20:23 -0300

    resolvi mais conflitos de merge 2

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
-  echo " 123 Só tem fera na Inffel!!!"
-  echo "Olá uuu";
-=======
-  echo "Só tem fera na Inffel!!!";
-    
-  echo "opsss";
->>>>>>> 41951d3d81af898d0d98a9cda631996347dd7689
+
+  echo " Só tem fera na Inffel!!!";
+  echo " Dîner 3.0 ";
+echo " git doidao ";
 
 ?>
